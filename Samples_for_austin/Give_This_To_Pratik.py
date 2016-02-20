e5acdac44c5bc8ae36294c68fc98c0563b0f24b4
VishalJagan <vij15@pitt.edu>
2016-02-20 13:59:08 -0500

    did changes

--- Samples_for_austin/Give_This_To_Pratik.py
+++ Samples_for_austin/Give_This_To_Pratik.py
@@ -12,6 +12,28 @@ i = 0
 q = 0;
 # Once file number is determined we open it.
 lookup = '%';
+
+def alexaSay(someArray):
+    #someArray = sub_array;
+    #print sub_array
+    if(len(someArray) != len(sub_array)):
+        print "Array size mismatch. The array you passed is not equal in length to the array I passed"
+    lineBuffer = '';
+    indexPos = 0;
+    lineBuffer = open(file,'r').read();
+    #Now must remove the percent things and replace with the strings from the array
+    #print lineBuffer
+    while lookup in lineBuffer:
+        #Kill the percents off
+        next_target = lineBuffer.find('%');
+        lineBuffer = lineBuffer[:next_target] + someArray[indexPos] +lineBuffer[(next_target+3):]
+        indexPos = indexPos + 1;
+    #print lineBuffer
+    #print '\n\nPercents removed'
+    return lineBuffer
+
+
+
 with open(file) as myFile:
         sub_array = []
         for num, line in enumerate(myFile, 1):
@@ -30,7 +52,7 @@ with open(file) as myFile:
                         continue
                     else:
                         break;
-        print sub_array
+        #print sub_array
         item = 0
         for element in sub_array:
             if('n' in sub_array[item]):
@@ -43,7 +65,16 @@ with open(file) as myFile:
             item = item+1;
 
 
-        print sub_array
+        #At this point, Echo will take in the sub_array which contains the parts of speech
+
+        #get user inputs
+
+        #Put into function below
+
+        alexaSay(sub_array); #This is how you pass your array of things to read to the program. Replace with the array you make, Pratik
+
+
+
 
 
 
