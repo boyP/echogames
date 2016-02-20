13fc847f513346d87e5c93c41b23cfd2c6335960
VishalJagan <vij15@pitt.edu>
2016-02-20 01:08:12 -0500

    did all that stuff but sub_array cuz screw 2d arrays

--- Samples_for_austin/Mad_Libs.py
+++ Samples_for_austin/Mad_Libs.py
@@ -7,8 +7,10 @@ elif randomInt == 2:
     file = "ML_2.txt"
 else:
     print "Broken"
+file = "ML_1.txt" #for now
 i = 0
 j = 0
+q=0;
 # Once file number is determined we open it.
 lookup = '%';
 with open(file) as myFile:
@@ -19,16 +21,25 @@ with open(file) as myFile:
             sub_array = [[0]*(len(line)+1),[0]*2]
             for i in xrange(len(line)):
                 if lookup in line:
-                    print lookup
-                    print type(lookup)
-                    print 'line =', line
-                    print 'line.find =', line.find(lookup)
-                    index_of_percent = int(line.find(lookup)) #Will give us the index
-                    print i, j, type(i), type(j)
-                    sub_array[i][j] = index_of_percent + 1
-                    sub_array[i][j + 1] = index_of_percent + 2
-                    continue
-
+                    index_of_percent = [i for i, x in enumerate(line) if x == lookup];
+                    print "Going through indexes"
+                    print "Indexes -> ", index_of_percent
+                    for element in index_of_percent:
+                        if((q != len(index_of_percent))):
+                            print "Element -> ",q
+                            print "For index ->", index_of_percent[q]
+                            print 'Text = ', line
+                            print 'Type -> ', line[index_of_percent[q]+1]
+                            print 'Unique ID -> ', line[index_of_percent[q]+2]
+                            print i, j, type(i), type(j)
+                            sub_array[i][j] = line[index_of_percent + 1];
+                            sub_array[i][j + 1] = line[index_of_percent + 2];
+                            i = i+1;
+                            q=q+1;
+                            continue
+                        else:
+                            print "BREAK";
+                            break;
 
 
 
