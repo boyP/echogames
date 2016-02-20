3754716f4a0008f1683c37712340d1abf95ee718
all141 <all141@pitt.edu>
2016-02-20 02:02:44 -0500

    Update Mad_Libs.py

--- Samples_for_austin/Mad_Libs.py
+++ Samples_for_austin/Mad_Libs.py
@@ -9,8 +9,7 @@ else:
     print "Broken"
 file = "ML_1.txt" #for now
 i = 0
-j = 0
-q=0;
+q = 0;
 # Once file number is determined we open it.
 lookup = '%';
 with open(file) as myFile:
@@ -18,24 +17,24 @@ with open(file) as myFile:
            #num is the line number. an integer
 	  #line is the line. a string
             #for i in enumerate (line, 1):
-            sub_array = [[0]*(len(line)+1),[0]*2]
+            sub_array = [[0]*(len(line)+1)]
             for i in xrange(len(line)):
                 if lookup in line:
-                    index_of_percent = [i for i, x in enumerate(line) if x == lookup];
+                    index_of_percent = [i for i,x in enumerate(line) if x == lookup];
                     print "Going through indexes"
                     print "Indexes -> ", index_of_percent
                     for element in index_of_percent:
-                        if((q != len(index_of_percent))):
+                        if(q != len(index_of_percent)):
                             print "Element -> ",q
                             print "For index ->", index_of_percent[q]
                             print 'Text = ', line
+                            print 'Line length = ', (len(line)+1)
                             print 'Type -> ', line[index_of_percent[q]+1]
-                            print 'Unique ID -> ', line[index_of_percent[q]+2]
-                            print i, j, type(i), type(j)
-                            sub_array[i][j] = line[index_of_percent + 1];
-                            sub_array[i][j + 1] = line[index_of_percent + 2];
+                            print i, type(i)
+                            sub_array[i] = line[index_of_percent[q] + 1];
+                            print 'array value = ', sub_array[i]
                             i = i+1;
-                            q=q+1;
+                            q = q+1;
                             continue
                         else:
                             print "BREAK";
